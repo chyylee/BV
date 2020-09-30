3218a3a93eaf38435d73bbb1e8537d5e87601897
chyylee <chyylee@umich.edu>
2020-09-30 09:16:58 -0400

    corrected ODE model to manuscript version

--- 2_sp_model/GvLi_ODE_function.m
+++ 2_sp_model/GvLi_ODE_function.m
@@ -27,11 +27,19 @@ MNZint_lb = y(4);
 MNZint_gv = y(5);
 MET = y(6);
 
-% Ordinary Differential Equations
+L_li = alpha_LB* MNZint_lb/(EC50_LB + MNZint_lb);
+L_gv = alpha_GV* MNZint_gv/(EC50 + MNZint_gv);
+cell_vol = 1E-7;
+cult_vol = 0.5;
+pc_Li = MNZint_lb/(LB*cult_vol);
+pc_Gv = MNZint_gv/(GV*cult_vol);
+
+% % Ordinary Differential Equations
 dydt=zeros(6,1);
-dydt(1) = beta_lb*LB*(1 - LB/K_lb) - alpha_LB* MNZint_lb/(EC50_LB + MNZint_lb)*LB; % Li
-dydt(2) = beta_GV*GV*(1 - GV/K) - alpha_GV* MNZint_gv/(EC50 + MNZint_gv)*GV; % Gv
-dydt(3) = - beta_in_lb*MNZext*LB - beta_in*MNZext*GV; %MNZ external
-dydt(4) = beta_in_lb*MNZext*LB; % Internal MNZ for LB
-dydt(5) = (beta_in*MNZext - kmet*MNZint_gv)*GV; % Internal MNZ for GV
+dydt(1) = beta_lb*LB*(1 - LB/K_lb) - L_li*LB; % Li
+dydt(2) = beta_GV*GV*(1 - GV/K) - L_gv*GV; % Gv
+dydt(3) = - beta_in_lb*MNZext*LB - beta_in*MNZext*GV + ...
+    L_li*LB*pc_Li*cell_vol + L_gv*GV*pc_Gv*cell_vol; %MNZ external
+dydt(4) = beta_in_lb*MNZext*LB - L_li*LB*pc_Li*cell_vol; % Internal MNZ for LB 
+dydt(5) = (beta_in*MNZext - kmet*MNZint_gv)*GV - L_gv*GV*pc_Gv*cell_vol; % Internal MNZ for GV - 
 dydt(6) = (kmet * MNZint_gv)*GV; % Metabolites
